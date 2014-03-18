aec6acff7f874d2dc89bea26674cb1d5c38ef838
Shengzhou Zhang <steven.shengzhou@gmail.com>
2014-03-18 22:14:24 +1100

    side bar

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zhang on 18/03/2014.
+ */
+(function launchFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+})(document.documentElement);
